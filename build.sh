02e57eab5d9bab35bfb239d34e1a3beb3781f31a
Shar Developer <0.shar.developer@gmail.com>
2021-08-07 16:46:28 +0300

    Added operator 'do'.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 sourceFiles=`ls src/*.shar`
 sharc -o sharc.ll -m /usr/include/shar/STD.sharm -s $sourceFiles main.shar
-opt -O3 --mattr=+avx2 sharc.ll -o sharc.bc
+opt -O3 -mcpu=x86-64-v3 sharc.ll -o sharc.bc
 rm sharc.ll
