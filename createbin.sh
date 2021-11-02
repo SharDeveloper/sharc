d96b10b5c323df34483e70f8ccb2003e7b44bf67
SharDeveloper <0.shar.developer@gmail.com>
2021-11-02 15:44:51 +0300

    When creating a binary, the clang no longer displays warnings.

--- createbin.sh
+++ createbin.sh
@@ -1,1 +1,1 @@
-clang -O1 -march=native -lm -ldl -pthread sharc.bc /usr/lib/libshar-os-api.so -o sharc
+clang -Wno-override-module -O1 -march=native -lm -ldl -pthread sharc.bc /usr/lib/libshar-os-api.so -o sharc
